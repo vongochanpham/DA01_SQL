7a143c9b9703bae4752961659da182bae24a4986
vongochanpham <iampavonohavv@gmail.com>
2024-05-28 21:26:38 -0500

    Create PRACTICE 4.sql

---
+++ PRACTICE 4.sql
@@ -0,0 +1,18 @@
+--ex1: datalemur-laptop-mobile-viewership.
+SELECT 
+    (SELECT COUNT(*) FROM VIEWERSHIP WHERE device_type = 'laptop') AS laptop_views,
+    (SELECT COUNT(*) FROM VIEWERSHIP WHERE device_type IN ('tablet', 'phone')) AS mobile_views;
+--ex2: datalemur-triangle-judgement.
+SELECT  x, y, z, 
+CASE WHEN x + y > z AND x + z > y AND y + z > x THEN 'Yes'
+ELSE 'No'
+END AS triangle
+FROM Triangle;
+--ex3: datalemur-uncategorized-calls-percentage.
+SELECT ROUND(100.0 * COUNT(*) FILTER(WHERE call_category IS NULL OR call_category = 'n/a')
+/ COUNT(*),1) AS uncategorised_call_pct 
+FROM callers;
+--ex4: datalemur-find-customer-referee.
+select name from customer
+where referee_id !=2 or referee_id is null;
+--ex5: stratascratch the-number-of-survivors.
