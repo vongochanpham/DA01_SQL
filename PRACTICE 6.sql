06491a5e52ccba86642c32344f6db011ea8802c2
vongochanpham <iampavonohavv@gmail.com>
2024-06-06 07:11:46 -0500

    Create PRACTICE 6.sql

---
+++ PRACTICE 6.sql
@@ -0,0 +1,13 @@
+ex1: datalemur-duplicate-job-listings.
+ex2: datalemur-highest-grossing.
+ex3: datalemur-frequent-callers.
+ex4: datalemur-page-with-no-likes.
+ex5: datalemur-user-retention.
+ex6: leetcode-monthly-transactions.
+ex7: leetcode-product-sales-analysis.
+ex8: leetcode-customers-who-bought-all-products.
+ex9: leetcode-employees-whose-manager-left-the-company.
+ex10: leetcode-primary-department-for-each-employee.
+ex11: leetcode-movie-rating.
+ex12: leetcode-who-has-the-most-friends.
+
