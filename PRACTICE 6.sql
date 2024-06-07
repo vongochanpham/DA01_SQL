bbb1b8878a0c301859e0d807a64f667db288fe27
vongochanpham <iampavonohavv@gmail.com>
2024-06-06 22:16:26 -0500

    Update PRACTICE 6.sql

--- PRACTICE 6.sql
+++ PRACTICE 6.sql
@@ -7,7 +7,31 @@ job_listings
 GROUP BY company_id
 HAVING COUNT(description) > 1) AS dupes;
 ex2: datalemur-highest-grossing.
+WITH RankedProducts AS (
+    SELECT
+        category,
+        product,
+        SUM(spend) AS total_spend,
+        RANK() OVER (PARTITION BY category ORDER BY SUM(spend) DESC) AS "rank"
+    FROM
+        product_spend
+    WHERE
+        EXTRACT(YEAR FROM transaction_date) = 2022
+    GROUP BY
+        category, product
+)
+SELECT
+    category,
+    product,
+    total_spend
+FROM
+    RankedProducts
+WHERE
+    "rank" <= 2
+ORDER BY
+    category, total_spend DESC;
 ex3: datalemur-frequent-callers.
+
 ex4: datalemur-page-with-no-likes.
 ex5: datalemur-user-retention.
 ex6: leetcode-monthly-transactions.
