ee25c172b53525572e9c5bf3fee1cce0382927a5
vongochanpham <iampavonohavv@gmail.com>
2024-06-08 16:13:40 -0500

    Update PRACTICE 6.sql

--- PRACTICE 6.sql
+++ PRACTICE 6.sql
@@ -6,6 +6,7 @@ COUNT(description) FROM
 job_listings 
 GROUP BY company_id
 HAVING COUNT(description) > 1) AS dupes;
+
 --ex2: datalemur-highest-grossing.
 WITH RankedProducts AS (
     SELECT
@@ -40,12 +41,14 @@ HAVING COUNT(case_id) >= 3)
 
 select count(policy_holder_id) as policy_holder_count
 from call_records;
+
 --ex4: datalemur-page-with-no-likes.
 SELECT p.page_id
 FROM pages p
 LEFT JOIN page_likes pl ON p.page_id = pl.page_id
 WHERE pl.page_id IS NULL
 ORDER BY p.page_id ASC;
+
 --ex5: datalemur-user-retention.
 with cte as (
 SELECT DISTINCT user_id FROM user_actions
@@ -57,6 +60,7 @@ where extract(month from event_date)=6
 and extract(year from event_date) = 2022)
 select '7' as month, count(user_id) as monthly_active_users
 from cte;
+
 --ex6: leetcode-monthly-transactions.
 SELECT DATE_FORMAT(trans_date, "%Y-%m") as month, 
 country, count(id) as trans_count, 
@@ -65,6 +69,7 @@ sum(amount) as trans_total_amount,
 sum(if(state = 'approved', amount, 0)) as approved_total_amount
 FROM Transactions
 group by country, month
+    
 --ex7: leetcode-product-sales-analysis.
 SELECT product_id, year AS first_year, quantity, price 
 FROM sales
@@ -73,13 +78,54 @@ WHERE (product_id, year) IN (
     FROM sales 
     GROUP BY product_id
 );
+
 --ex8: leetcode-customers-who-bought-all-products.
 SELECT  customer_id FROM Customer GROUP BY customer_id
 HAVING COUNT(distinct product_key) = (SELECT COUNT(product_key) FROM Product)
 
-ex9: leetcode-employees-whose-manager-left-the-company.
-    
-ex10: leetcode-primary-department-for-each-employee.
-ex11: leetcode-movie-rating.
-ex12: leetcode-who-has-the-most-friends.
+--ex9: leetcode-employees-whose-manager-left-the-company.
+select employee_id from Employees
+where salary < 30000
+and manager_id not in (select employee_id from Employees) 
+order by employee_id;
+     
+--ex10: leetcode-primary-department-for-each-employee.
+SELECT count(*) as duplicate_companies
+FROM (
+    SELECT company_id, count(description) FROM job_listings
+    GROUP BY company_id
+    HAVING count(description) > 1) as dupes;
+
+--ex11: leetcode-movie-rating.
+(SELECT name AS results
+FROM MovieRating JOIN Users USING(user_id)
+GROUP BY name
+ORDER BY COUNT(*) DESC, name
+LIMIT 1)
+
+UNION ALL
+
+(SELECT title AS results
+FROM MovieRating JOIN Movies USING(movie_id)
+WHERE EXTRACT(YEAR_MONTH FROM created_at) = 202002
+GROUP BY title
+ORDER BY AVG(rating) DESC, title
+LIMIT 1);
+
+--ex12: leetcode-who-has-the-most-friends.
+select id, count(*) as num from(
+select accepter_id as id from requestaccepted 
+union all select requester_id from requestaccepted
+) as friends_count
+group by id 
+ order by num desc limit 1
+--or
+with cte as (
+    select requester_id as id from RequestAccepted
+    UNION ALL
+    select accepter_id as id from RequestAccepted)
+select id, count(*) as num from cte 
+GROUP BY id
+order by num desc
+limit 1
 
